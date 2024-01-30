09372a8739af15b8565690048af636400683a6b2
SharanKumar <sharankumar.d@diggibyte.com>
2024-01-30 18:39:56 +0530

    updated the project

---
+++ pythonProject2/Sample1.py
@@ -0,0 +1,10 @@
+    # Create SparkSession object
+import org.apache.spark.sql.SparkSession
+    object SparkSessionTest extends App {
+      val spark = SparkSession.builder()
+          .master("local[1]")
+          .appName("SparkByExamples.com")
+          .getOrCreate();
+      println(spark)
+      println("Spark Version : "+spark.version)
+    }
